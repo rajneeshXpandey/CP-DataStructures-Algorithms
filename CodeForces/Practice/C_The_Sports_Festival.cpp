6321a664b8d60108b1d8ed57f5a82ef106a28a85
Rajneesh Pandey <rajneeshpandey1708@gmail.com>
2021-05-10 01:07:01 +0530

    Atcoder regular, CF and Adsa assignment

---
+++ CodeForces/Practice/C_The_Sports_Festival.cpp
@@ -0,0 +1,114 @@
+// #pragma GCC optimize ("O3")
+// #pragma GCC target ("sse4")
+#pragma GCC target("avx2")
+#pragma GCC optimize("Ofast")
+#include "bits/stdc++.h"
+using namespace std;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+
+template <typename A, typename B>
+string to_string(pair<A, B> p)
+{
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+template <size_t N>
+string to_string(bitset<N> v)
+{
+    string res = "";
+    for (size_t i = 0; i < N; i++)
+    {
+        res += static_cast<char>('0' + v[i]);
+    }
+    return res;
+}
+template <typename A>
+string to_string(A v)
+{
+    bool first = true;
+    string res = "{";
+    for (const auto &x : v)
+    {
+        if (!first)
+        {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+//   cout << fixed << setprecision(10);
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define per(i, a, b) for (int i = (a)-1; i >= (b); i--)
+#define forn(i, n) rep(i, 0, n)
+#define rof(i, n) per(i, n, 0)
+#define forone(i, n) for (int i = 1; i <= (n); ++i)
+#define dbg(x) cout << #x << "=" << x << endl
+#define dbg2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define dbg3(x, y, z) cout << #x << "=" << x << "," << #y << "=" << y << "," << #z << "=" << z << endl
+#define ff first
+#define ss second
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+#define int long long
+#define ll long long
+#define pb push_back
+#define pii pair<int, int>
+#define setbits(x) __builtin_popcountll(x)
+#define zerbefone(x) __builtin_ctzll(x)
+#define pqb priority_queue<int>                               // maxheap
+#define pqs priority_queue<int, vector<int>, greater<int>>    // minheap
+#define piipqs priority_queue<pii, vector<pii>, greater<pii>> // minheap for pair<int,int>
+#define piipqb priority_queue<pii>                            // maxheap for pair<int,int>
+#define mod 1000000007
+#define inf 100000000000000000 //1e17
+#define mem0(a) memset(a, 0, sizeof(a))
+#define mem1(a) memset(a, -1, sizeof(a))
+#define meminf(a) memset(a, 0x7f, sizeof(a))
+#define precise(x, y) fixed << setprecision(y) << x
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+// #define oset            tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+// mt19937_64 rng(61378913);
+/* usage - just do rng() */
+
+// *************************** Code Begins **************************** //
+int arr[2002];
+int mem[2002][2002];
+
+int dp(int l,int r)
+{  
+    if(l==r)
+        return mem[l][r] = 0;
+
+    if(mem[l][r-1]== -1)
+        mem[l][r - 1] = dp(l, r - 1);
+
+    if(mem[l+1][r]== -1)
+        mem[l+1][r] = dp(l+1, r);
+
+    return mem[l][r] = (arr[r] - arr[l] + min(mem[l][r-1],mem[l + 1][r]));
+}
+
+signed main()
+{
+    FIO;
+    mem1(mem);
+    int n;
+    cin>>n;
+    forn(i,n) cin>>arr[i];
+    sort(arr,arr+n);
+    cout<<dp(0,n-1)<<endl;
+}
