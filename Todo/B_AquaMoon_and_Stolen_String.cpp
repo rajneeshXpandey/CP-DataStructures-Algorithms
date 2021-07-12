4a05f50c8e3b504efd39f568c896f9f3eb33a4f9
RAJNEESH1708 <rajneeshpandey1708@gmail.com>
2021-07-13 03:35:43 +0530

    CSES-DP and CF Contest Problems added

--- Todo/B_AquaMoon_and_Stolen_String.cpp
+++ Todo/B_AquaMoon_and_Stolen_String.cpp
@@ -20,24 +20,21 @@ using namespace std;
 #define ss second
 #define mp make_pair
 #define all(x) (x).begin(), (x).end()
-#define endl "\n"
 #define int long long
 #define ll long long
 #define pb push_back
 #define pii pair<int, int>
-#define removeDuplicate(x) \
-    sort(all(x));          \
-    x.erase(unique(all(x)), x.end())       //remove Duplicate
-#define setbits(x) __builtin_popcountll(x) // count set bits in binary rep
+#define removeDuplicate(x)   sort(all(x)); x.erase(unique(all(x)),x.end())   //remove Duplicate
+#define setbits(x) __builtin_popcountll(x)      // count set bits in binary rep
 #define zerbefone(x) __builtin_ctzll(x)
 #define pqb priority_queue<int>                               // maxheap
 #define pqs priority_queue<int, vector<int>, greater<int>>    // minheap
 #define piipqs priority_queue<pii, vector<pii>, greater<pii>> // minheap for pair<int,int>
 #define piipqb priority_queue<pii>                            // maxheap for pair<int,int>
-#define mod 1000000007                                        //1e9+7
+#define mod 1000000007   //1e9+7
 #define mod1 998244353
 #define inf 2000000000000000000 //2e18
-#define PI 3.141592653589793238
+#define PI  3.141592653589793238
 #define mem0(a) memset(a, 0, sizeof(a))
 #define mem1(a) memset(a, -1, sizeof(a))
 #define meminf(a) memset(a, 0x7f, sizeof(a))
@@ -54,59 +51,56 @@ mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
 /* usage - just do rng() */
 
 //assign and update min and max values.
-template <typename T, typename T1>
-T amax(T &a, T1 b)
-{
-    if (b > a)
-        a = b;
-    return a;
-}
-template <typename T, typename T1>
-T amin(T &a, T1 b)
-{
-    if (b < a)
-        a = b;
-    return a;
-}
+template<typename T, typename T1> T amax(T &a, T1 b) {if (b > a)a = b; return a;}
+template<typename T, typename T1> T amin(T &a, T1 b) {if (b < a)a = b; return a;}
 
-inline ll gcd(ll a, ll b) { return (b == 0) ? a : gcd(b, a % b); }
+inline ll gcd(ll a, ll b){return (b==0)?a:gcd(b,a%b);}
 
 // *********************** Code Begins ************************ //
 
 void solve()
-{
-    int n, x,v;
-    cin>>n>>x;
-    multiset<int> arr;
-    forn(i, n)
-           { cin >>v;
-           arr.insert(v);
-           }
-    int count = 0;
-    int j=0;
-    while(!arr.empty())
-    {
-            auto idx = lower_bound(arr.begin(), arr.end(), (x - *arr.begin()));
-            if(idx==arr.begin())
-             break;
-            else{
-                count++;
-                arr.erase(lower_bound(arr.begin(), arr.end(),*idx));
-                arr.erase(lower_bound(arr.begin(), arr.end(),*arr.begin()));
-            } 
+{  
+    int n,m;
+    cin>>n>>m;
+    vector<string> v(n);
+    string s,a;
+    map<char,int> mp;
+    map<string,string> strmap;
+    forn(i,n){
+        cin>>v[i];
+        a = v[i];
+        sort(all(a));
+        strmap[a] = v[i];
+        forn(j,m)
+           mp[v[i][j]]++;
     }
-    deb(count);
-    cout<<arr.size()+count<<'\n';
+    forn(i,n-1){
+        cin>>s;
+        forn(j,m)
+          { if(mp[s[j]]==1)
+              mp.erase(s[j]);
+            else 
+              mp[s[j]]--; 
+          }
+    }
+    string last = "";
+    for(auto m: mp)
+      forn(i,m.ss)
+         last +=  m.ff;
+
+     cout<<strmap[last]<<endl;
+     fflush(stdout);    
+    
 }
-//  8 8 8 9 9 10
-//  3 7
+
 signed main()
 {
     FastIO;
     int tt = 1;
+    cin >> tt;
     for (int i = 1; i <= tt; i++)
-    {
-        // cout<<"Case #"<< i <<": ";
-        solve();
-    }
+        {            
+         // cout<<"Case #"<< i <<": "; 
+         solve();
+        }        
 }
