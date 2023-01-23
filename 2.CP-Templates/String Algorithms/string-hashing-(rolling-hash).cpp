8d8f734c685b7368d6642b4f1f5e361131c837b7
Rajneesh Pandey <rajneeshpandey1708@gmail.com>
2023-01-24 03:29:19 +0530

    string and trie notes added

--- 2.CP-Templates/String Algorithms/string-hashing-(rolling-hash).cpp
+++ 2.CP-Templates/String Algorithms/string-hashing-(rolling-hash).cpp
@@ -1,5 +1,10 @@
 
 //1-base //better to use 2 hashes
+
+/*
+    const int p = 31;
+    const int m = 1e9 + 9;
+*/
 int power(int x, int y, int p) {
     int res = 1;
     x = x % p;
@@ -11,30 +16,28 @@ int power(int x, int y, int p) {
     }
     return res;
 }
-int modi(int a, int m) {
+int modinv(int a, int m) {
     return power(a, m - 2, m);
 }
 struct Hash{
-    vector<int>pref;
-    vector<int>pows;
-    vector<int>i_pows;
+    vector<int> pref,powers,inv_powers;
     int p,m;
     Hash(string &s,int _p,int _m): p(_p) , m(_m)
     {
-        int n=sz(s);
-        pows.resize(n+1,0);
+        int n=s.size();
+        powers.resize(n+1,0);
         pref.resize(n+1,0);
-        i_pows.resize(n+1,0);
-        pows[0]=1;
-        int p_inv=modi(p,m);
-        i_pows[0]=1;
+        inv_powers.resize(n+1,0);
+        powers[0]=1;
+        int p_inv=modinv(p,m);
+        inv_powers[0]=1;
         for(int i=1;i<=n;i++){
-            pref[i]=(pref[i-1]+(s[i-1]-'a'+1)*pows[i-1])%m;
-            pows[i]=(pows[i-1]*p)%m;
-            i_pows[i]=(i_pows[i-1]*p_inv)%m;
+            pref[i]=(pref[i-1]+(s[i-1]-'a'+1)*powers[i-1])%m;
+            powers[i]=(powers[i-1]*p)%m;
+            inv_powers[i]=(inv_powers[i-1]*p_inv)%m;
         }
     }
     int get(int l,int r){
-        return ((pref[r]-pref[l-1]+m)*i_pows[l-1])%m;
+        return ((pref[r]-pref[l-1]+m)*inv_powers[l-1])%m;
     }
 };
